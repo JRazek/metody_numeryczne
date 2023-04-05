5606ccb326b5c32816607c14bcbe354e81279073
JRazek <jakub.razek2@gmail.com>
2023-04-05 02:46:04 +0200

    added meanQuantity, formatter for Measurement

--- src/uncertainties/uncertainties.hpp
+++ src/uncertainties/uncertainties.hpp
@@ -124,7 +124,7 @@ auto addUncertainties(ld& uncertainties_combined, auto const& function, Quantiti
 }  // namespace implementation
 
 template <std::same_as<Quantity>... Quantities>
-auto combineMeasurements(auto const& function, Quantities const&... quantities) -> Quantity {
+auto combineQuantities(auto const& function, Quantities const&... quantities) -> Quantity {
   auto values = std::make_tuple(quantities.value_...);
 
   auto values_combined = std::apply(function, values);
@@ -139,7 +139,43 @@ auto combineMeasurements(auto const& function, Quantities const&... quantities) 
   };
 }
 
+inline auto meanQuantity(std::vector<Quantity> const& quantities) -> Quantity {
+  auto mean = ld{};
+  auto combined_uncertainty_sq = ld{};
+  for (auto const& [value, uncertainty_sq] : quantities) {
+    mean += value;
+    combined_uncertainty_sq += uncertainty_sq;
+  }
+
+  mean /= quantities.size();
+  combined_uncertainty_sq /= (quantities.size() * quantities.size());
+
+  return Quantity{
+      mean,
+      combined_uncertainty_sq,
+  };
+}
+
 }  // namespace uncertainty
+
+template <>
+struct fmt::formatter<uncertainty::Measurement> {
+  template <typename ParseContext>
+  constexpr auto parse(ParseContext& ctx) {
+    return ctx.begin();
+  }
+
+  template <typename FormatContext>
+  auto format(uncertainty::Measurement const& quantity, FormatContext& ctx) {
+    return format_to(
+        ctx.out(),
+        "mean: {:.3}, std_deviation: {:.3}, std_uncertainty_of_mean: {:.3}, generalized_uncertainty: {:.3}",
+        quantity.mean_,
+        std::sqrt(quantity.variance_),
+        std::sqrt(quantity.std_uncertainty_of_mean_sq_),
+        std::sqrt(quantity.generalized_uncertainty_sq_));
+  }
+};
 
 template <>
 struct fmt::formatter<uncertainty::Quantity> {
@@ -153,68 +189,3 @@ struct fmt::formatter<uncertainty::Quantity> {
     return format_to(ctx.out(), "value: {:.3} \\pm {:.3}", quantity.value_, std::sqrt(quantity.uncertainty_sq_));
   }
 };
-
-//auto main() -> int {  // NOLINT
-//  using uncertainty::combineMeasurements;
-//  using uncertainty::Measurement;
-//  using uncertainty::partialDerivative;
-//  using uncertainty::Quantity;
-//  using uncertainty::setupMeasurement;
-//  using utils::ld;
-//
-//  auto a = static_cast<Quantity>(setupMeasurement(utils::readDataset("a.txt"), 0.01));
-//  auto d = static_cast<Quantity>(setupMeasurement(utils::readDataset("d.txt"), 0.00002));
-//
-//  auto h1 = static_cast<Quantity>(setupMeasurement(utils::readDataset("h1.txt"), 0.01));
-//  auto l1 = combineMeasurements([](ld a, ld h, ld d) { return a - h - d; }, a, h1, d);
-//  auto tx1h1 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x1h1.txt"), 0.01));
-//  auto tx2h1 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x2h1.txt"), 0.01));
-//  auto tx5h1 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x5h1.txt"), 0.01));
-//  auto tx10h1 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x10h1.txt"), 0.01));
-//
-//  auto h2 = static_cast<Quantity>(setupMeasurement(utils::readDataset("h2.txt"), 0.01));
-//  auto l2 = combineMeasurements([](ld a, ld h, ld d) { return a - h - d; }, a, h2, d);
-//  auto tx10h2 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x10h2.txt"), 0.01));
-//
-//  auto h3 = static_cast<Quantity>(setupMeasurement(utils::readDataset("h3.txt"), 0.01));
-//  auto l3 = combineMeasurements([](ld a, ld h, ld d) { return a - h - d; }, a, h3, d);
-//  auto tx10h3 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x10h3.txt"), 0.01));
-//
-//  auto h4 = static_cast<Quantity>(setupMeasurement(utils::readDataset("h4.txt"), 0.01));
-//  auto l4 = combineMeasurements([](ld a, ld h, ld d) { return a - h - d; }, a, h4, d);
-//  auto tx10h4 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x10h4.txt"), 0.01));
-//
-//  auto h5 = static_cast<Quantity>(setupMeasurement(utils::readDataset("h5.txt"), 0.01));
-//  auto l5 = combineMeasurements([](ld a, ld h, ld d) { return a - h - d; }, a, h5, d);
-//  auto tx10h5 = static_cast<Quantity>(setupMeasurement(utils::readDataset("x10h5.txt"), 0.01));
-//
-//  //  fmt::print("h1: {}\n", h1);
-//  //  fmt::print("tx10h1: {}\n\n", tx10h1);
-//  //
-//  //  fmt::print("h2: {}\n", h2);
-//  //  fmt::print("tx10h2: {}\n\n", tx10h2);
-//  //
-//  //  fmt::print("h3: {}\n", h3);
-//  //  fmt::print("tx10h3: {}\n\n", tx10h3);
-//  //
-//  //  fmt::print("h4: {}\n", h4);
-//  //  fmt::print("tx10h4: {}\n\n", tx10h4);
-//  //
-//  //  fmt::print("h5: {}\n", h5);
-//  //  fmt::print("tx10h5: {}\n\n", tx10h5);
-//
-//  fmt::print("h1: {}\n", h1);
-//  fmt::print("l1: {}\n\n", l1);
-//
-//  fmt::print("h2: {}\n", h2);
-//  fmt::print("l2: {}\n\n", l2);
-//
-//  fmt::print("h3: {}\n", h3);
-//  fmt::print("l3: {}\n\n", l3);
-//
-//  fmt::print("h4: {}\n", h4);
-//  fmt::print("l4: {}\n\n", l4);
-//
-//  fmt::print("h5: {}\n", h5);
-//  fmt::print("l5: {}\n\n", l5);
-//}
